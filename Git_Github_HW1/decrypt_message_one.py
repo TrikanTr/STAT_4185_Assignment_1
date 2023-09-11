76bf3cfa9461e5e61c4a175106dbc37a2086f3ca
trikantr <pratham.patel@uconn.edu>
2023-09-10 20:55:49 -0400

    created and switched to code one branch, created code and solved message one

---
+++ Git_Github_HW1/decrypt_message_one.py
@@ -0,0 +1,58 @@
+cipher = {
+    'a':'v',
+    'b':'h',
+    'c':'r',
+    'd':'j',
+    'e':'t',
+    'f':'x',
+    'g':'s',
+    'h':'a',
+    'i':'e',
+    'j':'f',
+    'k':'b',
+    'l':'n',
+    'm':'o',
+    'n':'i',
+    'o':'g',
+    'p':'l',
+    'q':'m',
+    'r':'z',
+    's':'q',
+    't':'u',
+    'u':'c',
+    'v':'k',
+    'w':'d',
+    'x':'p',
+    'y':'y',
+    'z':'w',
+    ' ': '}',
+    '\n': '^',
+    ',': '5',
+    '!': '[',
+    ':':'-',
+    ')':'*',
+    '.': '%' 
+}
+
+encrypted_file = open("encrypted_message_one.txt", 'r')
+
+encrypted_message = encrypted_file.readline()
+
+encrypted_file.close()
+
+# Write code below
+def get_key(val):
+    for key, value in cipher.items():
+        if val == value:
+            return key
+
+msg_letters = [x for x in encrypted_message]
+
+answer_ltrs = []
+for i in msg_letters:
+    msg_key = get_key(i)
+    answer_ltrs.append(msg_key)
+
+answer = "".join(answer_ltrs)
+print(answer)
+    
