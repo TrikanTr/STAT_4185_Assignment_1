54d75ab2e1ca101cc1f9de96d605bda114927d6a
trikantr <pratham.patel@uconn.edu>
2023-09-10 21:33:40 -0400

    created and switched to msg two, coded and solved message two

--- Git_Github_HW (1)/decrypt_message_two.py
+++ Git_Github_HW (1)/decrypt_message_two.py
@@ -5,3 +5,15 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+msg_ltrs = [x for x in encrypted_message]
+i = 1
+len = len(msg_ltrs)
+while (i < (len//2)):
+    slot1 = msg_ltrs[i]
+    slot2 = msg_ltrs[len-i-1]
+    msg_ltrs[i] = slot2
+    msg_ltrs[len-i-1] = slot1
+    i += 2
+
+answer = "".join(msg_ltrs)
+print(answer)
